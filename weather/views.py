66701008b0de2a6fbbea9f3c6f98c0faa1958afa
kibet kilel <amos.rotich@strathmore.edu>
2021-08-17 18:16:29 +0300

    update

--- weather/views.py
+++ weather/views.py
@@ -6,7 +6,7 @@ import urllib.request
 def index(request):
     if request.method == 'POST':
         city = request.POST['city']
-        res = urllib.request.urlopen('hq='+city+'&').read()
+        res = urllib.request.urlopen('http://api.openweathermap.org/data/2.5/weather?q='+city+'&appid=cb771e45ac79a4e8e2205c0ce66ff633').read()
         json_data = json.loads(res)
         data = {
             "country_code": str(json_data['sys']['country']),
